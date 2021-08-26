c9fc57e93eeb99d5ce6ce3b3fcea12505e85541e
Matt Kane <matt@gatsbyjs.com>
2021-08-26 13:06:09 +0100

    Fix patch script

--- patch-gatsby.sh
+++ patch-gatsby.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 echo Patching gatsby packages
-find node_modules/gatsby* -type f -name '*.js' -print0 | xargs -0 sed -i '' 's/"" === `4`/"4" === `4`/'
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+    find node_modules/gatsby* -type f -name '*.js' -print0 | xargs -0 sed -i '' 's/"" === `4`/"4" === `4`/'
+else
+    find node_modules/gatsby* -type f -name '*.js' -print0 | xargs -0 sed -i 's/"" === `4`/"4" === `4`/' 
+fi
