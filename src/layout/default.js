7e5a9621d3fb6f4e3c1087ee71170aa8a7165a2b
Matt Kane <m@mk.gg>
2021-09-16 15:06:01 +0100

    Update demo

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,16 +1,16 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import { headerCss, titleCss } from "./default.module.css";
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { headerCss, titleCss } from './default.module.css'
 
 export function Layout({ children }) {
   return (
     <>
       <header className={headerCss}>
         <h1 className={titleCss}>
-          <Link to="/">SSR Demo</Link>
+          <Link to="/">Gatsby 4 Demo</Link>
         </h1>
       </header>
       <main>{children}</main>
     </>
-  );
+  )
 }
