f2340be5b8f857ce6a659f171830ee08d564deb8
Matt Kane <m@mk.gg>
2021-09-17 09:16:56 +0100

    Update utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,63 @@
-import { join } from "path";
-import os from "os";
-import { existsSync, copySync, emptyDirSync } from "fs-extra";
-import { link } from "linkfs";
-import fs from "fs";
-import type { GraphQLEngine } from "gatsby/cache-dir/query-engine";
+import fs from 'fs'
+import os from 'os'
+import { join } from 'path'
+import process from 'process'
 
-export const CACHE_DIR = join(process.cwd(), `.cache`);
-export const TEMP_CACHE_DIR = join(os.tmpdir(), "gatsby", ".cache");
+import { existsSync, copySync } from 'fs-extra'
+// eslint-disable-next-line node/no-unpublished-import
+import type { GraphQLEngine } from 'gatsby/cache-dir/query-engine'
+import { link } from 'linkfs'
 
-export function prepareFilesystem(includedDirs: Array<string>) {
-  const rewrites = [
-    [join(CACHE_DIR, "caches"), join(TEMP_CACHE_DIR, "caches")],
-    [join(CACHE_DIR, "caches-lmdb"), join(TEMP_CACHE_DIR, "caches-lmdb")],
-    [join(CACHE_DIR, "data"), join(TEMP_CACHE_DIR, "data")],
-    [join(process.cwd(), "public"), join(os.tmpdir(), "gatsby", "public")],
-  ];
-  console.log(rewrites);
-  const lfs = link(fs, rewrites);
-  for (const key in lfs) {
-    if (Object.hasOwnProperty.call(fs[key], "native")) {
-      lfs[key].native = fs[key].native;
+// Bundled with the function, but readonly
+export const CACHE_DIR = join(process.cwd(), `.cache`)
+
+// Alias in the temp directory so it's writable
+export const TEMP_CACHE_DIR = join(os.tmpdir(), 'gatsby', '.cache')
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      _fsWrapper: typeof import('fs')
     }
   }
-  global._fsWrapper = lfs;
-  emptyDirSync(TEMP_CACHE_DIR);
+}
 
-  includedDirs.forEach((dir) => {
-    if (!existsSync(join(TEMP_CACHE_DIR, dir))) {
-      console.time(`Copying ${dir}`);
-      copySync(join(CACHE_DIR, dir), join(TEMP_CACHE_DIR, dir));
-      console.timeEnd(`Copying ${dir}`);
+/**
+ * Hacks to deal with the fact that functions execute on a readonly filesystem
+ */
+export function prepareFilesystem(): void {
+  const rewrites = [
+    [join(CACHE_DIR, 'caches'), join(TEMP_CACHE_DIR, 'caches')],
+    [join(CACHE_DIR, 'caches-lmdb'), join(TEMP_CACHE_DIR, 'caches-lmdb')],
+    [join(CACHE_DIR, 'data'), join(TEMP_CACHE_DIR, 'data')],
+  ]
+  // Alias the cache dir paths to the temp dir
+  const lfs = link(fs, rewrites) as typeof import('fs')
+
+  // linkfs doesn't pass across the `native` prop, which graceful-fs needs
+  for (const key in lfs) {
+    if (Object.hasOwnProperty.call(fs[key], 'native')) {
+      lfs[key].native = fs[key].native
     }
-  });
+  }
+  // Gatsby uses this instead of fs if present
+  // eslint-disable-next-line no-underscore-dangle
+  global._fsWrapper = lfs
+  const dir = 'data'
+  if (!process.env.NETLIFY_LOCAL && existsSync(join(TEMP_CACHE_DIR, dir))) {
+    console.log('directory already exists')
+    return
+  }
+  console.log(`Start copying ${dir}`)
+  copySync(join(CACHE_DIR, dir), join(TEMP_CACHE_DIR, dir))
+  console.log(`End copying ${dir}`)
 }
 
 // Inlined from gatsby-core-utils
 
 export function reverseFixedPagePath(pageDataRequestPath: string): string {
-  return pageDataRequestPath === `index` ? `/` : pageDataRequestPath;
+  return pageDataRequestPath === `index` ? `/` : pageDataRequestPath
 }
 
 export function getPagePathFromPageDataPath(
@@ -45,23 +65,29 @@ export function getPagePathFromPageDataPath(
 ): string | null {
   const matches = pageDataPath.matchAll(
     /^\/?page-data\/(.+)\/page-data.json$/gm
-  );
+  )
 
+  // Not sure why Gatsby does this!
+  // eslint-disable-next-line no-unreachable-loop
   for (const [, requestedPagePath] of matches) {
-    return reverseFixedPagePath(requestedPagePath);
+    return reverseFixedPagePath(requestedPagePath)
   }
 
-  return null;
+  return null
 }
 
-export function getGraphQLEngine() {
-  const { GraphQLEngine: GQE } = require(join(CACHE_DIR, "query-engine")) as {
-    GraphQLEngine: typeof GraphQLEngine;
-  };
+/**
+ * Loads the bundled GraphQL engine from the Gatsby cache directory
+ */
+export function getGraphQLEngine(): GraphQLEngine {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, node/global-require
+  const { GraphQLEngine: GQE } = require(join(CACHE_DIR, 'query-engine')) as {
+    GraphQLEngine: typeof GraphQLEngine
+  }
 
-  const dbPath = join(TEMP_CACHE_DIR, "data", "datastore");
+  const dbPath = join(TEMP_CACHE_DIR, 'data', 'datastore')
 
   return new GQE({
     dbPath,
-  });
+  })
 }
